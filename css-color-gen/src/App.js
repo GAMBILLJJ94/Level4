0f43051ba059475a37d221ccfbe9a435b3aae28d
Joseph Gambill <joey.gambill94@gmail.com>
2022-07-08 20:09:32 -0400

    finished css generator

--- css-color-gen/src/App.js
+++ css-color-gen/src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
 import React from 'react'
-import Gradient from './Gradient';
+import Options from './Options';
 
 function App() {
+
+  
+  const [light, setLight] = React.useState(true)
+
+  function toggleDarkLight(){setLight(!light)}
+
+  const darkMode = {
+    backgroundColor:"darkgrey",
+    color:"azure"
+  }
+  const lightMode = {
+    backgroundColor:"azure",
+    color:"black"
+  }
+
+
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor:(light ? "azure" : "darkgrey"),color:(light ? "black" : "azure")}}>
+      <button type="checkbox" className='darkSwitch' onClick={toggleDarkLight} >Light/Dark</button>
+
       <h1 className="header" >CSS Gradient Code Generator</h1>
-      <Gradient>
-        <textarea className='gradientBox' readOnly />
-        <textarea className='cssCode' readOnly />
-      </Gradient>
+      <Options/>
     </div>
   );
 }
--- css-color-gen/src/Gradient.js
+++ css-color-gen/src/Gradient.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import Options from './Options'
+export default function Gradient (props) {
 
-export default function Gradient () {
+
+
     return (
-        <div className='body'>
         <div className='leftBox'>
-            <div className='gradient'></div>
-            <textarea readOnly/>
-        </div>
-        <Options/>
+            <div style={{background: `linear-gradient(${props.angleOf}deg,${props.col1},${props.col2})`}} className='gradient'></div>
+            <textarea value={`background : linear-gradient(${props.angleOf}deg, ${props.col1} , ${props.col2}); 
+-moz-background: linear-gradient(50deg, ${props.col1} , ${props.col2}); 
+-webkit: linear-gradient(50deg, ${props.col1} , ${props.col2})`} readOnly></textarea>
         </div>
     )
 }
